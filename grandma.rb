72fb7af782a91e4ec7521b1fb54cb0bbd6c0446f
Brandon Williams <nis350ztt@gmail.com>
2020-07-09 03:38:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   
   if phrase.downcase == phrase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase = "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
   else
     "NO, NOT SINCE 1938!"
